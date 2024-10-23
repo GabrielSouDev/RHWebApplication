b60a3efaae09b2b29162ad9d16f27d89fe446c61
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2024-10-23 08:09:08 -0300

    deletingAspire

--- RHWebApplication.AppHost/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var API = builder.AddProject<Projects.RHWebApplication_API>("rhwebapplication-api");
-builder.AddProject<Projects.RHWebApplication_Web>("rhwebapplication-web").WithReference(API);
-
-builder.Build().Run();
